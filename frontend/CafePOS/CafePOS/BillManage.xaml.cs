2e1bbf25415a4dd87ee10c3a21583e4741c818d8
ndbao1802 <147065695+Duybao1802@users.noreply.github.com>
2025-04-02 03:02:53 +0700

    Finish manage bills

--- frontend/CafePOS/CafePOS/BillManage.xaml.cs
+++ frontend/CafePOS/CafePOS/BillManage.xaml.cs
@@ -1,12 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.UI.Xaml;
 using CafePOS.DAO;
 using CafePOS.DTO;
-using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Protection.PlayReady;
+
 
 namespace CafePOS.Views
 {
@@ -22,7 +35,6 @@ namespace CafePOS.Views
 
         private async void LoadDefaultBills()
         {
-            // Load bills for today by default
             DateTime today = DateTime.Today;
             CheckInDatePicker.Date = today;
             CheckOutDatePicker.Date = today;
@@ -41,21 +53,44 @@ namespace CafePOS.Views
             DateTime checkOut = CheckOutDatePicker.Date.DateTime;
             await LoadBillsByDate(checkIn, checkOut);
         }
-
         private async Task LoadBillsByDate(DateTime checkIn, DateTime checkOut)
         {
-            var bills = await BillDAO.Instance.GetBillsByDateAsync(checkIn, checkOut);
-            if (bills != null && bills.Count > 0)
+            var client = DataProvider.Instance.Client;
+            var response = await client.GetAllBills.ExecuteAsync();
+
+            if (response.Data?.AllBills?.Edges == null)
             {
-                Bills = bills;
+                Debug.WriteLine("Error: No data received from GraphQL.");
+                return;
+            }
+
+            var allBills = response.Data.AllBills.Edges
+                .Select(edge => new Bill(
+                    edge.Node.Id,
+                    DateTime.TryParse(edge.Node.DateCheckIn, out DateTime checkIn) ? checkIn : (DateTime?)null,
+                    DateTime.TryParse(edge.Node.DateCheckOut, out DateTime checkOut) ? checkOut : (DateTime?)null,
+                    edge.Node.Status
+                ))
+                .ToList();
+
+            var filteredBills = allBills
+                .Where(bill => bill.DateCheckIn.HasValue &&
+                               bill.DateCheckIn.Value.Date >= checkIn.Date &&
+                               bill.DateCheckIn.Value.Date <= checkOut.Date)
+                .ToList();
+
+            if (filteredBills.Any())
+            {
+                Bills = filteredBills;
                 BillListView.ItemsSource = Bills;
             }
             else
             {
                 Debug.WriteLine("No bills found for the selected date range.");
+                Bills.Clear();
+                BillListView.ItemsSource = null;
             }
         }
-
 
         private async void CheckOutButton_Click(object sender, RoutedEventArgs e)
         {
